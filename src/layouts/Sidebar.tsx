8ef6d537b0c0ce40ecb256f24aba04a8eecb9775
soulivanh <kokkok@Soulivanhs-MacBook-Pro.local>
2024-01-19 16:49:39 +0700

    add blue screen on sidebar open

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -30,7 +30,7 @@ import { useSidebarContext } from "../contexts/SidebarContext";
 import { PageHeaderFirstSection } from "./PageHeder";
 
 export function Sidebar() {
-  const { isLargeOpen, isSmallOpen } = useSidebarContext();
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext();
 
   return (
     <>
@@ -48,12 +48,20 @@ export function Sidebar() {
         />
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
+
+      {isSmallOpen && (
+        <div
+          onClick={close}
+          className="lg:hidden fixed inset-0 z-[999] bg-secondary-dark opacity-50"
+        />
+      )}
+
       <aside
         className={`w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-4  ${
           isLargeOpen ? "lg:flex" : "lg:hidden"
         } ${isSmallOpen ? "flex z-[999] bg-white mix-h-screen" : "hidden"}`}
       >
-        <div className="lg:hidden pt-2 pb-4 px-2">
+        <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
           <PageHeaderFirstSection hidden={false} />
         </div>
         <LargeSidebarSection>
